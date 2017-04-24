3e009691c6594182041ce6dc5c97ce75d0362c8b
zhenying.diao <1710127116@qq.com>
2017-04-24 19:38:28 +0800

    my session

---
+++ src/dSession/Session.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package dSession;
+
+/**
+ * TODO:Description the class
+ * 
+ * @author zhenying.diao(diaozhenying@tcl.com) 2017Äê4ÔÂ24ÈÕ
+ * 
+ */
+public class Session {
+	private String sessionId;
+
+}
